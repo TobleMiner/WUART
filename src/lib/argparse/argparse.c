67a7159eb374645ae597d4b1bf4c78e2cfa913f3
Tobias Schramm <tobleminer@gmail.com>
2014-11-09 16:08:49 +0100

    Added licensing information

--- src/lib/argparse/argparse.c
+++ src/lib/argparse/argparse.c
@@ -1,6 +1,25 @@
 /*
-<BLACK MAGIC>
+Copyright (c) 2014 Tobias Schramm
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in
+all copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+THE SOFTWARE.
 */
+
 extern uint8_t args_split(char* str, char*** args)
 {
 	char* allocated = NULL;
@@ -67,9 +86,6 @@ extern uint8_t args_split(char* str, char*** args)
 	free(allocated);
 	return argcnt;
 }
-/*
-</BLACK MAGIC>
-*/
 
 extern void args_free(char** args, uint8_t argnum)
 {
